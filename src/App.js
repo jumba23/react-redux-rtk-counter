d4b47a1b7e67359537dab17624436b5bfc729045
Goran SD <g.cvetic23@gamail.com>
2022-12-07 17:43:57 -0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import React from 'react';
+import { Counter } from './features/counter/counter';
 import logo from './logo.svg';
 import './App.css';
 
@@ -6,17 +8,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Counter />
+        
       </header>
     </div>
   );
